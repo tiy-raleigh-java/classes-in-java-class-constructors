ebf6988373b0ec0d68135ad82de5f99096ed01a6
Rickie Cashwell <rjc6056@gmail.com>
2017-03-06 11:56:25 -0500

    commit

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -15,17 +15,33 @@
 public class Character {
 
     // todo: Create a property to hold the character's name
-
+    String name;
     // todo: Create a property to hold the character's type. This will be free-form text.
-
+    String type;
     // todo: create a property to hold the character's health. This should be a double value.
-
+    double health;
     // todo: Create a property to hold the character's power. This should be a double value.
-
+    double power;
     // todo: Create a constructor that accepts and sets the default values for the name, type, health, and power properties. Note that the order specified is required for tests to pass.
-
+    public Character(String name, String type, double health, double power){
+        this.name = name;
+        this.type = type;
+        this.health = health;
+        this.power = power;
+    }
     // todo: Create a method named 'attack'. This method should accept a Character that will be attacked by this character.
+    public void attack(Character player1, Character player2){
+    
 
+        player1 = new Character(this.name,this.type,this.health,this.power);
+        player2 = new Character(player2.name,player2.type,player2.health,player2.power);
+        System.out.println(player1.name +" attacks " +player2.name);
+        player2.health = player2.health - player1.power;
+        System.out.println(player2.name+" health is: " +player2.health);
+
+
+
+    }
         // todo: Update the character that was passed into the attack() method by decreasing its health by this character's power.
         /*
             As an example for this to-do, imagine this character is a wizard
@@ -40,4 +56,8 @@ public class Character {
         power of 23 would output "The wizard Lisa has 100.0 health and deals 23
         damage on each attack."
      */
+    public String toString(){
+        String status = "The "+ type+ " "+name+" has " +health+" health and deals "+ power+ " damage on each attack";
+        return status;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,21 +7,26 @@
 public class Main {
 
     // todo: Create a main() method.
-
+    public static void main(String[] args) {
         // todo: Create a new instance of the Character class named wizard. The instance's type should be set to 'wizard', but all other properties are up to you.
+        Character wizard = new Character("Rickie", "Wizard", 1000, 25);
 
         // todo: Create a new instance of the Character class named orc. The instance's type should be set to 'orc', but all other properties are up to you.
-
+        Character orc = new Character("Hugo", "Orc", 1000, 2);
         // todo: Print the wizard to the console.
+            System.out.println(wizard);
+            // todo: Print the orc to the console
+            System.out.println(orc);
+            // todo: Have the wizard attack the orc
+            wizard.attack(wizard, orc);
+            // todo: Have the orc attack the wizard
+            orc.attack(orc, wizard);
+            // todo: Print the wizard to the console again.
+            System.out.println(wizard);
+            // todo: Print the orc to the console again.
+            System.out.println(orc);
+    }
+    }
 
-        // todo: Print the orc to the console.
 
-        // todo: Have the wizard attack the orc
 
-        // todo: Have the orc attack the wizard
-
-        // todo: Print the wizard to the console again.
-
-        // todo: Print the orc to the console again.
-
-}
